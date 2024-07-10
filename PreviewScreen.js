f1bc3d4a7200382c04c2bbca33738be053c394a2
Fordjour Gyasi Samuel <sgfordjour@st.ug.edu.gh>
2024-07-10 08:27:06 +0000

    Initial commit for files and photos

---
+++ PreviewScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PreviewScreen = () => {
+  return (
+    <View>
+      <Text>PreviewScreen</Text>
+    </View>
+  )
+}
+
+export default PreviewScreen
+
+const styles = StyleSheet.create({})
---
+++ src/context/CartContext.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CartContext = () => {
+  return (
+    <View>
+      <Text>CartContext</Text>
+    </View>
+  )
+}
+
+export default CartContext
+
+const styles = StyleSheet.create({})
