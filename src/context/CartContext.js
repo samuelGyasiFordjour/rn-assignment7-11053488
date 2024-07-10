b65a491a5d2b6410470f69ceea3db685eace8f75
Fordjour Gyasi Samuel <sgfordjour@st.ug.edu.gh>
2024-07-10 10:31:06 +0000

    Updated the cart context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { createContext, useState } from 'react';
 
-const CartContext = () => {
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCart(cart.filter(item => item.id !== productId));
+  };
+
+  const totalPrice = () => {
+    return cart.reduce((total, item) => total + item.price, 0).toFixed(2);
+  };
+
   return (
-    <View>
-      <Text>CartContext</Text>
-    </View>
-  )
-}
-
-export default CartContext
-
-const styles = StyleSheet.create({})
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, totalPrice }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
